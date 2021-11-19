e13b18ae24f3525f19244725fd1711aa1811a686
aqntks <cjd0030@gmail.com>
2021-11-20 01:02:03 +0900

    nv5 nv6 nv7

---
+++ nv5.py
@@ -0,0 +1,204 @@
+import pandas as pd
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+from tqdm import tqdm_notebook as tqdm # progress bar
+from datetime import datetime
+import time
+import matplotlib.pyplot as plt
+from pycocotools.coco import COCO
+import os, json, cv2, random
+import skimage.io as io
+import copy
+from pathlib import Path
+from typing import Optional
+
+from tqdm import tqdm
+import itertools
+
+import torch
+import albumentations as A
+from albumentations.pytorch.transforms import ToTensorV2
+
+from glob import glob
+import numba
+from numba import jit
+
+import warnings
+warnings.filterwarnings('ignore') #Ignore "future" warnings and Data-Frame-Slicing warnings.
+
+
+# detectron2
+from detectron2.structures import BoxMode
+from detectron2 import model_zoo
+from detectron2.config import get_cfg
+from detectron2.data import DatasetCatalog, MetadataCatalog
+from detectron2.engine import DefaultPredictor, DefaultTrainer, launch
+from detectron2.evaluation import COCOEvaluator
+from detectron2.structures import BoxMode
+from detectron2.utils.visualizer import ColorMode
+from detectron2.utils.logger import setup_logger
+from detectron2.utils.visualizer import Visualizer
+
+from detectron2.data import DatasetCatalog, MetadataCatalog, build_detection_test_loader, build_detection_train_loader
+from detectron2.data import detection_utils as utils
+
+
+from detectron2.data import DatasetCatalog, MetadataCatalog, build_detection_test_loader, build_detection_train_loader
+from detectron2.data import detection_utils as utils
+import detectron2.data.transforms as T
+from detectron2.evaluation import COCOEvaluator, inference_on_dataset
+
+setup_logger()
+
+Data_Resister_training = "sartorius_train"
+Data_Resister_valid = "sartorius_val"
+from detectron2.data.datasets import register_coco_instances
+dataDir = Path('./')
+
+register_coco_instances(Data_Resister_training,{}, 'coco_cell_train_fold5.json', dataDir)
+register_coco_instances(Data_Resister_valid,{},'coco_cell_valid_fold5.json', dataDir)
+
+metadata = MetadataCatalog.get(Data_Resister_training)
+dataset_train = DatasetCatalog.get(Data_Resister_training)
+dataset_valid = DatasetCatalog.get(Data_Resister_valid)
+
+
+# ## Augmentation ######################################
+def custom_mapper(dataset_dict):
+    dataset_dict = copy.deepcopy(dataset_dict)
+    image = utils.read_image(dataset_dict["file_name"], format="BGR")
+    transform_list = [
+            T.RandomCrop(crop_type='relative_range', crop_size=[0.5, 0.5]),
+            T.ResizeShortestEdge(short_edge_length=(640, 672, 704, 736, 768, 800), max_size=1333, sample_style='choice'),
+            T.RandomFlip(prob=0.5, horizontal=False, vertical=True),
+            T.RandomFlip(prob=0.5, horizontal=True, vertical=False),
+            # T.RandomApply(T.RandomRotation((-10, 10)), prob=0.2),
+            T.RandomSaturation(0.9, 1.1),
+            T.RandomBrightness(0.9, 1.1),
+            T.RandomContrast(0.9, 1.1),
+            T.RandomLighting(0.9),
+    ]
+    image, transforms = T.apply_transform_gens(transform_list, image)
+    dataset_dict["image"] = torch.as_tensor(image.transpose(2, 0, 1).astype("float32"))
+
+    annos = [
+        utils.transform_instance_annotations(obj, transforms, image.shape[:2])
+        for obj in dataset_dict.pop("annotations")
+        if obj.get("iscrowd", 0) == 0
+    ]
+    instances = utils.annotations_to_instances(annos, image.shape[:2], mask_format="bitmask")
+    dataset_dict["instances"] = utils.filter_empty_instances(instances)
+    return dataset_dict
+
+class AugTrainer(DefaultTrainer):
+    @classmethod
+    def build_train_loader(cls, cfg):
+        return build_detection_train_loader(cfg, mapper=custom_mapper)
+
+    @classmethod
+    def build_evaluator(cls, cfg, dataset_name, output_folder=None):
+        return MAPIOUEvaluator(dataset_name)
+
+
+# ## Evaluator ######################################
+
+from detectron2.evaluation.evaluator import DatasetEvaluator
+import pycocotools.mask as mask_util
+
+
+def precision_at(threshold, iou):
+    matches = iou > threshold
+    true_positives = np.sum(matches, axis=1) == 1  # Correct objects
+    false_positives = np.sum(matches, axis=0) == 0  # Missed objects
+    false_negatives = np.sum(matches, axis=1) == 0  # Extra objects
+    return np.sum(true_positives), np.sum(false_positives), np.sum(false_negatives)
+
+
+def score(pred, targ):
+    pred_masks = pred['instances'].pred_masks.cpu().numpy()
+    enc_preds = [mask_util.encode(np.asarray(p, order='F')) for p in pred_masks]
+    enc_targs = list(map(lambda x: x['segmentation'], targ))
+    ious = mask_util.iou(enc_preds, enc_targs, [0] * len(enc_targs))
+    prec = []
+    for t in np.arange(0.5, 1.0, 0.05):
+        tp, fp, fn = precision_at(t, ious)
+        p = tp / (tp + fp + fn)
+        prec.append(p)
+    return np.mean(prec)
+
+
+class MAPIOUEvaluator(DatasetEvaluator):
+    def __init__(self, dataset_name):
+        dataset_dicts = DatasetCatalog.get(dataset_name)
+        self.annotations_cache = {item['image_id']: item['annotations'] for item in dataset_dicts}
+
+    def reset(self):
+        self.scores = []
+
+    def process(self, inputs, outputs):
+        for inp, out in zip(inputs, outputs):
+            if len(out['instances']) == 0:
+                self.scores.append(0)
+            else:
+                targ = self.annotations_cache[inp['image_id']]
+                self.scores.append(score(out, targ))
+
+    def evaluate(self):
+        return {"MaP IoU": np.mean(self.scores)}
+
+
+class Trainer(DefaultTrainer):
+    @classmethod
+    def build_evaluator(cls, cfg, dataset_name, output_folder=None):
+        return MAPIOUEvaluator(dataset_name)
+
+
+# ## Training
+cfg = get_cfg()
+cfg.MODEL.DEVICE = "cuda:0"
+config_name = "Misc/cascade_mask_rcnn_X_152_32x8d_FPN_IN5k_gn_dconv.yaml"
+cfg.merge_from_file(model_zoo.get_config_file(config_name))
+cfg.DATASETS.TRAIN = (Data_Resister_training,)
+cfg.DATASETS.TEST = (Data_Resister_valid,)
+
+cfg.MODEL.WEIGHTS = "nv/model16_0014999.pth"
+
+# cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(config_name)
+
+cfg.DATALOADER.NUM_WORKERS = 2
+cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 64  # 64 is slower but more accurate (128 faster but less accurate)
+cfg.MODEL.ROI_HEADS.NUM_CLASSES = 3
+cfg.SOLVER.IMS_PER_BATCH = 2  # (2 is per defaults)
+cfg.INPUT.MASK_FORMAT = 'bitmask'
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5
+
+cfg.SOLVER.BASE_LR = 0.0005  # (quite high base learning rate but should drop)
+# cfg.SOLVER.MOMENTUM = 0.9
+# cfg.SOLVER.WEIGHT_DECAY = 0.0005
+# cfg.SOLVER.GAMMA = 0.1
+
+
+cfg.SOLVER.WARMUP_ITERS = 10  # How many iterations to go from 0 to reach base LR
+cfg.SOLVER.MAX_ITER = 20000  # Maximum of iterations 1
+cfg.SOLVER.STEPS = (500, 1000)  # At which point to change the LR 0.25,0.5
+cfg.TEST.EVAL_PERIOD = 250
+cfg.SOLVER.CHECKPOINT_PERIOD = 250
+
+cfg.OUTPUT_DIR = "./nv/nv5"
+
+os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
+trainer = AugTrainer(cfg) # with  data augmentation
+# trainer = Trainer(cfg)  # without data augmentation
+trainer.resume_or_load(resume=False)
+trainer.train()
+
+# ## Evaluator ######################################
+evaluator = COCOEvaluator(Data_Resister_valid, cfg, False, output_dir="./nv/nv5/")
+cfg.MODEL.WEIGHTS= "nv/nv5/model_final.pth"
+#cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.2   # set a custom testing threshold
+#cfg.INPUT.MASK_FORMAT='polygon'
+val_loader = build_detection_test_loader(cfg, Data_Resister_valid)
+inference_on_dataset(trainer.model, val_loader, evaluator)
+
+
